056efce59fe53c3e7ff0ef9d40f4e8e844645f6a
mmarovich <mmarovich@gmail.com>
2016-12-26 13:36:34 -0600

    v1

---
+++ app.js
@@ -0,0 +1,50 @@
+state = {
+	cards: {
+		currentCard: '',
+		id: '',
+		drawn1: '',
+		drawn2: '',
+		pile1: '',
+		pile2: ''
+	},
+	urls: {
+		shuffle: 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1',
+		draw: '',
+		pile1: '',
+		pile2: ''
+	}
+}
+
+
+function getDataFromApi(url, callback){
+	$.getJSON(url, callback);
+}
+
+
+
+$(document).ready(function(){
+	
+// cards are shuffled and distributed evenly between two players.
+	$('.shuffle').click(function(e){
+		getDataFromApi(state.urls.shuffle, function(data){
+			state.cards.id = data.deck_id;
+			state.urls.draw = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/draw/?count=26';	
+			state.urls.pile2 = 'https://deckofcardsapi.com/api/deck/' + 
+			state.cards.id + '/pile/pile_2/add/';
+		drawToPile();
+		drawToPile();
+		$('.shuffle').addClass('hidden');
+		$('.attack').removeClass('hidden');
+		});
+		function drawToPile(){
+			getDataFromApi(state.urls.draw, function(data){
+				state.urls.pile1 = 'https://deckofcardsapi.com/api/deck/' + 
+				state.cards.id + '/pile/pile_1/add/';
+				getDataFromApi(state.urls.pile1, function(data){
+					state.cards.pile1 = data.cards;
+					console.log(state.cards.pile1);
+				})
+			})
+		}
+	});
+});
