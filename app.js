6c209a9ec8dedeee21eccf91c38dd814e6506a0b
mmarovich <mmarovich@gmail.com>
2016-12-28 15:10:19 -0600

    v5

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ state = {
 		attack2: '',
 		discard1: '',
 		discard2: ''
+	},
+	images: {
+		cardBack: 'image/card back red.png'
 	}
 }
 
@@ -26,8 +29,6 @@ state = {
 function getDataFromApi(url, callback){
 	$.getJSON(url, callback);
 }
-
-
 
 $(document).ready(function(){
 	
@@ -42,59 +43,76 @@ $(document).ready(function(){
 		});
 		function drawToPile1(){
 			getDataFromApi(state.urls.draw, function(data){
-				console.log(data);
+				var codeString = data.cards.map(function(card){
+					return card.code;
+				}).join();
+				data.cards.forEach(function(item, index){
+					$('.startDeck').append('<div class="card"><img class="card" src="image/card back red.png" style="z-index:' + index + 
+						'; margin-left:' + (index / 2) + 'px; margin-bottom:' + (index / 2) + 'px"></div>');
+				})
 				state.urls.pile1 = 'https://deckofcardsapi.com/api/deck/' + 
-				state.cards.id + '/pile/pile_1/add/?cards=' + data.cards[0].code + ',' + data.cards[1].code + 
-				',' + data.cards[2].code + ',' + data.cards[3].code + ',' + data.cards[4].code + ',' + data.cards[5].code +
-				',' + data.cards[6].code + ',' + data.cards[7].code + ',' + data.cards[8].code + ',' + data.cards[9].code + 
-				',' + data.cards[10].code + ',' + data.cards[11].code + ',' + data.cards[12].code + ',' + data.cards[13].code + 
-				',' + data.cards[14].code + ',' + data.cards[15].code + ',' + data.cards[16].code + ',' + data.cards[17].code + 
-				',' + data.cards[18].code + ',' + data.cards[19].code + ',' + data.cards[20].code + ',' + data.cards[21].code + 
-				',' + data.cards[22].code + ',' + data.cards[23].code + ',' + data.cards[24].code + ',' + data.cards[25].code;
+				state.cards.id + '/pile/pile_1/add/?cards=' + codeString;
 				getDataFromApi(state.urls.pile1, function(data){
 					state.cards.pile1 = data.piles.pile_1;
-					console.log(state.cards.pile1);
-					})
 				})
-			}
+			})
+		};
 		function drawToPile2(){
 			getDataFromApi(state.urls.draw, function(data){
+				var codeString = data.cards.map(function(card){
+					return card.code;
+				}).join();
 				state.urls.pile2 = 'https://deckofcardsapi.com/api/deck/' + 
-				state.cards.id + '/pile/pile_2/add/?cards=' + data.cards[0].code + ',' + data.cards[1].code + 
-				',' + data.cards[2].code + ',' + data.cards[3].code + ',' + data.cards[4].code + ',' + data.cards[5].code +
-				',' + data.cards[6].code + ',' + data.cards[7].code + ',' + data.cards[8].code + ',' + data.cards[9].code + 
-				',' + data.cards[10].code + ',' + data.cards[11].code + ',' + data.cards[12].code + ',' + data.cards[13].code + 
-				',' + data.cards[14].code + ',' + data.cards[15].code + ',' + data.cards[16].code + ',' + data.cards[17].code + 
-				',' + data.cards[18].code + ',' + data.cards[19].code + ',' + data.cards[20].code + ',' + data.cards[21].code + 
-				',' + data.cards[22].code + ',' + data.cards[23].code + ',' + data.cards[24].code + ',' + data.cards[25].code;
+				state.cards.id + '/pile/pile_2/add/?cards=' + codeString;
 				getDataFromApi(state.urls.pile2, function(data){
 					state.cards.pile2 = data.piles.pile_2;
-					console.log(state.cards.pile2);
 				})
 			});
 		}
-		$('.attack').click(function(e){
-			state.urls.attack1 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/pile_1/draw/'
-			state.urls.attack2 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/pile_2/draw/'
-			getDataFromApi(state.urls.attack1, function(data){
-				state.cards.currentVal1 = data.cards[0].value;
-				state.cards.currentCard1 = data.cards[0].code;
-			})
-			getDataFromApi(state.urls.attack2, function(data){
-				state.cards.currentVal2 = data.cards[0].value;
-				state.cards.currentCard2 = data.cards[0].code;
-			})
-			state.urls.discard1 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/discard_1/add/?cards=' + state.cards.currentCard1 + ',' + state.cards.currentCard2;
-			state.urls.discard2 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/discard_2/add/?cards=' + state.cards.currentCard1 + ',' + state.cards.currentCard2;
-			if (state.cards.currentVal1 > state.cards.currentVal2){
-				getDataFromApi(state.urls.discard1, function(data){
-					console.log(data);
-				})
-			} else if (state.cards.currentVal1 < state.cards.currentVal2) {
-				getDataFromApi(state.urls.discard2, function(data){
-					console.log(data);
-				})
-			}
-		})
 	});
+	$('.attack').click(function(e){
+		state.urls.attack1 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/pile_1/draw/';
+		state.urls.attack2 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/pile_2/draw/';
+		getDataFromApi(state.urls.attack1, function(data){
+			if (data.cards[0].value === "JACK"){
+				state.cards.currentVal1 = "11";
+			} else if (data.cards[0].value === "QUEEN"){
+				state.cards.currentVal1 = "12";
+			} else if (data.cards[0].value === "KING"){
+				state.cards.currentVal1 = "13";
+			} else if (data.cards[0].value === "ACE"){
+				state.cards.currentVal1 = "14";
+			} else {
+				state.cards.currentVal1 = data.cards[0].value;
+			}
+			state.cards.currentCard1 = data.cards[0].code;
+			console.log(state.cards.currentVal1);
+		})
+		getDataFromApi(state.urls.attack2, function(data){
+			if (data.cards[0].value === "JACK"){
+				state.cards.currentVal2 = "11";
+			} else if (data.cards[0].value === "QUEEN"){
+				state.cards.currentVal2 = "12";
+			} else if (data.cards[0].value === "KING"){
+				state.cards.currentVal2 = "13";
+			} else if (data.cards[0].value === "ACE"){
+				state.cards.currentVal2 = "14";
+			} else {
+				state.cards.currentVal2 = data.cards[0].value;
+			}
+			state.cards.currentCard2 = data.cards[0].code;
+			console.log(state.cards.currentVal2);
+		})
+		state.urls.discard1 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/discard_1/add/?cards=' + state.cards.currentCard1 + ',' + state.cards.currentCard2;
+		state.urls.discard2 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/discard_2/add/?cards=' + state.cards.currentCard1 + ',' + state.cards.currentCard2;
+		if (state.cards.currentVal1 > state.cards.currentVal2){
+			getDataFromApi(state.urls.discard1, function(data){
+				console.log(data);
+			})
+		} else if (state.cards.currentVal1 < state.cards.currentVal2) {
+			getDataFromApi(state.urls.discard2, function(data){
+				console.log(data)
+			})
+		}
+	})
 });
