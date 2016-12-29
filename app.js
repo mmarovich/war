d43de70164692cd8472302d2986b0f83f805b8d1
mmarovich <mmarovich@gmail.com>
2016-12-29 13:59:40 -0600

    v11

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ state = {
 		currentVal1: '',
 		currentVal2: '',
 		id: '',
-		drawn1: '',
-		drawn2: '',
+		drawn1: [],
+		drawn2: [],
 		pile1: '',
-		pile2: ''
+		pile2: '',
+		wagerCards: ""
 	},
 	urls: {
 		shuffle: 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1',
@@ -30,6 +31,20 @@ function getDataFromApi(url, callback){
 	$.getJSON(url, callback);
 }
 
+function cardValue(card){
+	if (card === "JACK"){
+		return 11;
+	} else if (card === "QUEEN"){
+		return 12;
+	} else if (card === "KING"){
+		return 13;
+	} else if (card === "ACE"){
+		return 14;
+	} else {
+		return parseInt(card);
+	}
+}
+
 $(document).ready(function(){
 	
 	$('.shuffle').click(function(e){
@@ -43,6 +58,7 @@ $(document).ready(function(){
 		});
 		function drawToPile1(){
 			getDataFromApi(state.urls.draw, function(data){
+				console.log(data);
 				var codeString = data.cards.map(function(card){
 					return card.code;
 				}).join();
@@ -62,6 +78,10 @@ $(document).ready(function(){
 				var codeString = data.cards.map(function(card){
 					return card.code;
 				}).join();
+				data.cards.forEach(function(item, index){
+					$('.startDeck').append('<div class="card"><img class="card" src="image/card back red.png" style="z-index:' + (index + 126) + 
+						'; margin-left:' + (index / 2) + 'px; margin-bottom:' + (index / 2) + 'px"></div>');
+				});
 				state.urls.pile2 = 'https://deckofcardsapi.com/api/deck/' + 
 				state.cards.id + '/pile/pile_2/add/?cards=' + codeString;
 				getDataFromApi(state.urls.pile2, function(data){
@@ -74,60 +94,50 @@ $(document).ready(function(){
 		state.urls.attack1 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/pile_1/draw/';
 		state.urls.attack2 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/pile_2/draw/';
 		getDataFromApi(state.urls.attack1, function(data){
-			if (data.cards[0].value === "JACK"){
-				state.cards.currentVal1 = "11";
-			} else if (data.cards[0].value === "QUEEN"){
-				state.cards.currentVal1 = "12";
-			} else if (data.cards[0].value === "KING"){
-				state.cards.currentVal1 = "13";
-			} else if (data.cards[0].value === "ACE"){
-				state.cards.currentVal1 = "14";
-			} else {
-				state.cards.currentVal1 = data.cards[0].value;
-			}
+			state.cards.currentVal1 = cardValue(data.cards[0].value);
 			state.cards.currentCard1 = data.cards[0].code;
 			console.log(state.cards.currentVal1);
-		})
-		getDataFromApi(state.urls.attack2, function(data){
-			if (data.cards[0].value === "JACK"){
-				state.cards.currentVal2 = "11";
-			} else if (data.cards[0].value === "QUEEN"){
-				state.cards.currentVal2 = "12";
-			} else if (data.cards[0].value === "KING"){
-				state.cards.currentVal2 = "13";
-			} else if (data.cards[0].value === "ACE"){
-				state.cards.currentVal2 = "14";
-			} else {
-				state.cards.currentVal2 = data.cards[0].value;
-			}
-			state.cards.currentCard2 = data.cards[0].code;
-			console.log(state.cards.currentVal2);
-			state.urls.discard1 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/discard_1/add/?cards=' + state.cards.currentCard1 + ',' + state.cards.currentCard2;
-			state.urls.discard2 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/discard_2/add/?cards=' + state.cards.currentCard1 + ',' + state.cards.currentCard2;
-			state.urls.war1 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/pile_1/draw/';
-			state.urls.war2 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/pile_2/draw/';
-			if (state.cards.currentVal1 > state.cards.currentVal2){
-				getDataFromApi(state.urls.discard1, function(data){
-					console.log(data);
-				})
-			} else if (state.cards.currentVal1 < state.cards.currentVal2) {
-				getDataFromApi(state.urls.discard2, function(data){
-					console.log(data)
-				})
-			} else {
-				getDataFromApi(state.urls.war1, function(data){
-					console.log(data)
-				})
-				getDataFromApi(state.urls.war1, function(data){
-					console.log(data)
-				})
-				getDataFromApi(state.urls.war2, function(data){
-					console.log(data)
-				})
-				getDataFromApi(state.urls.war2, function(data){
-					console.log(data)
-				})
-			}			
+			getDataFromApi(state.urls.attack2, function(data){
+				state.cards.currentVal2 = cardValue(data.cards[0].value);
+				state.cards.currentCard2 = data.cards[0].code;
+				console.log(state.cards.currentVal2);
+				state.urls.discard1 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/discard_1/add/?cards=' + state.cards.currentCard1 + ',' + state.cards.currentCard2 + "," + state.cards.wagerCards;
+				state.urls.discard2 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/discard_2/add/?cards=' + state.cards.currentCard1 + ',' + state.cards.currentCard2 + "," + state.cards.wagerCards;
+				state.urls.war1 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/pile_1/draw/';
+				state.urls.war2 = 'https://deckofcardsapi.com/api/deck/' + state.cards.id + '/pile/pile_2/draw/';
+				if (state.cards.currentVal1 > state.cards.currentVal2){
+					getDataFromApi(state.urls.discard1, function(data){
+						console.log(data);
+					})
+				} else if (state.cards.currentVal1 < state.cards.currentVal2) {
+					getDataFromApi(state.urls.discard2, function(data){
+						console.log(data)
+					})
+				} else {
+					$.when(
+					$.getJSON(state.urls.war1, function(data){
+						state.cards.drawn1.push(data.cards[0].code);
+					}),
+					$.getJSON(state.urls.war1, function(data){
+						state.cards.drawn1.push(data.cards[0].code);
+					}),
+					$.getJSON(state.urls.war2, function(data){
+						state.cards.drawn2.push(data.cards[0].code);
+					}),
+					$.getJSON(state.urls.war2, function(data){
+						state.cards.drawn2.push(data.cards[0].code);
+					})
+					).then(function(){
+						// display drawn cards
+						state.cards.wagerCards += (state.cards.wagerCards.length > 0 ? "," : "") + state.cards.currentCard1 + "," + state.cards.currentCard2 + "," + state.cards.drawn1.join() + "," + state.cards.drawn2.join();
+						state.cards.drawn1 = [];
+						state.cards.drawn2 = [];
+						state.cards.currentCard1 = "";
+						state.cards.currentCard2 = "";
+						console.log(state.cards.wagerCards);
+					})
+				}			
+			})
 		})
 	})
 });
